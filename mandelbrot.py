602e4c1a1f62485458acf745a8806f9b171a78e2
Javix5118 <javier2008martin@gmail.com>
2025-05-07 22:15:36 +0200

    1.1.0

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,32 +1,37 @@
 import turtle
 
 screen = turtle.Screen()
-WIDTH, HEIGHT = 800,800
+WIDTH = int(1.75 * 600)  # → 1050
+HEIGHT = 600
+
 
 PIXEL_SIZE = 3
-screen.bgcolor("darkblue")
+screen.bgcolor("white")
 screen.setup(WIDTH, HEIGHT)
 screen.title('Mandelbrot - Summa_in_primis')
 screen.tracer(0)
 t = turtle.Turtle()
 t.penup()
-RE_START, RE_END = -1, 1
+RE_START, RE_END = -2.5, 1
 IM_START, IM_END = -1, 1
 MAX_ITER = 100  # Iteraciones por punto
-
 
 '''
 Este es el algoritmo que determina si un número se encuentra en el conjunto de Mandelbrot o no,
 para esto se hace una sucesión hasta MAX_ITER, si el resultado de realizar este algoritmo da
 2 se dice que cae hacia el infinito asi que no se considera dentro del conjunto
 '''
+
+
 def mandelbrot(c):
+    global n
     z = 0
     for n in range(MAX_ITER):
         z = z ** 2 + c
         if abs(z) > 2:
-            return False
-    return True
+            return n
+
+    return n
 
 
 for x in range(-WIDTH // 2, WIDTH // 2, PIXEL_SIZE):
@@ -36,10 +41,33 @@ for x in range(-WIDTH // 2, WIDTH // 2, PIXEL_SIZE):
         im = IM_START + (y + HEIGHT // 2) * (IM_END - IM_START) / HEIGHT
         c = complex(re, im)
         m = mandelbrot(c)
-        if mandelbrot(c):
+        if mandelbrot(c) == 99:
+            t.color('black')
             t.goto(x, y)
             t.dot(PIXEL_SIZE)
+        elif mandelbrot(c) != 100:
+            if n <= 5:
+                t.color('red')
+                t.goto(x, y)
+                t.dot(PIXEL_SIZE)
+            elif n <= 8:
+                t.color('yellow')
+                t.goto(x, y)
+                t.dot(PIXEL_SIZE)
+            elif n <= 15:
+                t.color('green')
+                t.goto(x, y)
+                t.dot(PIXEL_SIZE)
+            elif n < 100:
+                t.color('blue')
+                t.goto(x, y)
+                t.dot(PIXEL_SIZE)
 
+    counter = 0
+    if counter == 100:
+        screen.update()
+        counter = 0
+    counter += 1
     screen.update()
 
 turtle.done()
